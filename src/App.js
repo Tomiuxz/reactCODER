97ccfd98d14551a70eaad0db7c977cde8dc663f7
Tomas <tomas_pedrini@hotmail.com>
2022-01-16 16:09:31 -0300

    itemlist añadido

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from "./logo.svg";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
+import { ItemListContainer } from "./components/CartWidget/ItemListContainer";
 
 function App() {
-	return (
-		<div className="App">
-			<Navbar />
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Welcome to Gaming Gods!</p>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navbar />
+      <ItemListContainer greetings="Hola!" />
+      <header className="App-header">
+        <h2>Welcome to Gaming Gods!</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CartWidget/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ItemListContainer = ({ greetings }) => {
+	// console.log(props);
+
+	return (
+		<div>
+			<h1>ITEMLIST</h1>
+			<p>{greetings}</p>
+		</div>
+	);
+};
